70c772ed5ae11f6ab450a19c8126cb2603bbcfc8
whisky81 <anh666666anh@gmail.com>
2025-06-14 10:25:29 +0700

    Smart Contract holding ETH

--- contracts/Whisky.sol
+++ contracts/Whisky.sol
@@ -25,6 +25,8 @@ contract Whisky is ERC721, ERC721Burnable {
     mapping(uint256 tokenId => Asset) private _assets;
     uint256 private _availableAssetCount;
 
+    mapping(address account => uint256) private _balances;
+
     /**
      * @dev Minting: The price provided for the minting operation is less than the minimum required (e.g., 0.005 ETH).
      * This error indicates that the input `price` does not meet the contract's minimum pricing criteria for a token.
@@ -39,6 +41,8 @@ contract Whisky is ERC721, ERC721Burnable {
      * @param amount The insufficient amount of Ether sent by the account, in wei.
      */
     error WhiskyInsufficientPayment(uint256 price, uint256 amount);
+
+    error WhiskyTransferFailed(address to, uint256 amount);
 
     constructor()
         ERC721("Whisky", "W")
@@ -60,16 +64,15 @@ contract Whisky is ERC721, ERC721Burnable {
         return "";
     }
 
-    function safeMint(string memory name, string memory cid) public payable returns (uint256) {
-        if (msg.value < 0.005 ether) {
-            revert WhiskyInvalidPrice(msg.value);
-        }
-
+    /**
+     * @dev Users can freely create their own NFTs, and the default value for each NFT is 0.005 ETH.
+     *
+    */
+    function safeMint(string memory name, string memory cid) public returns (uint256) {
         uint256 tokenId = _nextTokenId++;
         _availableAssetCount++;
 
-        uint256 price = (msg.value * 110) / 100;
-        Asset memory asset = Asset({ price: price, status: AssetStatus.Available, name: name, cid: cid });
+        Asset memory asset = Asset({ price: 0.005 ether, status: AssetStatus.Available, name: name, cid: cid });
         _assets[tokenId] = asset;
 
         _safeMint(_msgSender(), tokenId);
@@ -97,6 +100,7 @@ contract Whisky is ERC721, ERC721Burnable {
         address owner = _requireOwned(tokenId);
 
         Asset memory asset = _assets[tokenId];
+
         if (asset.price > msg.value) {
             revert WhiskyInsufficientPayment(asset.price, msg.value); 
         }
@@ -104,12 +108,16 @@ contract Whisky is ERC721, ERC721Burnable {
         _availableAssetCount--;
         _assets[tokenId].status = AssetStatus.Archived;
 
-        _safeTransfer(_ownerOf(tokenId), _msgSender(), tokenId);   
+        _safeTransfer(_ownerOf(tokenId), _msgSender(), tokenId, "");
 
-        if (asset.price < msg.value) {
-            payable(_msgSender()).transfer(msg.value - asset.price);
+        if (msg.value > asset.price) {
+            _balances[_msgSender()] += (msg.value - asset.price);
         }
-        payable(owner).transfer(asset.price);
+
+        (bool success, ) = payable(owner).call{value: asset.price}("");
+        if (!success) {
+            revert WhiskyTransferFailed(owner, asset.price);
+        }
 
         return true;     
     }
@@ -120,7 +128,8 @@ contract Whisky is ERC721, ERC721Burnable {
         uint256 idx = 0;
         for (uint256 i = 0; i < _nextTokenId; i++) {
             if (_isAvailableToken(i)) {
-                availableAssets[idx++] = _assets[i];
+                availableAssets[idx] = _assets[i];
+                idx++;
             }
         }
         return availableAssets;
@@ -132,11 +141,34 @@ contract Whisky is ERC721, ERC721Burnable {
 
         for (uint256 i = 0; i < _nextTokenId; i++) {
             if (_ownerOf(i) == account) {
-                assets[idx++] = _assets[i];
+                assets[idx] = _assets[i];
+                idx++;
             }
         }
         return assets;
     }
+
+    function balance() external view returns (uint256) {
+        return _balances[_msgSender()];
+    }
+
+    /**
+     *  @dev Withdraws the balance of the caller.
+     */
+    function withdraw() external {
+        uint256 amount = _balances[_msgSender()];
+        if (amount == 0) {
+            return;
+        }
+
+        _balances[_msgSender()] = 0;
+
+        (bool success, ) = payable(_msgSender()).call{value: amount}("");
+        if (!success) {
+            revert WhiskyTransferFailed(_msgSender(), amount);
+        }
+    }
+
     function _isAvailableToken(uint256 tokenId) internal view returns (bool) {
         return _ownerOf(tokenId) != address(0) && _assets[tokenId].status == AssetStatus.Available; 
     } 
